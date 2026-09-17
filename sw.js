405b09408099148a1fff0da449f8cd5d5fa09785
srisha24x7-netizen <srisha24x7@gmail.com>
2026-09-17 19:48:05 +0530

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = "my-week-v1";
+const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./sw.js"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
+  );
+});
